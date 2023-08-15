7b512f4e301e4a9e3e206058f04d08b032f6fae8
Maryna Khmarska <111706884+myhappyday@users.noreply.github.com>
2023-08-15 07:21:57 +0300

    Code Splitting

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,13 @@
+import { lazy } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
-import Home from 'pages/Home';
-import Movies from 'pages/Movies';
-import MovieDetails from 'pages/MovieDetails';
 import Layout from './Layout';
-import Cast from './Cast';
-import Reviews from './Reviews';
+
+const Home = lazy(() => import('../pages/Home'));
+const Movies = lazy(() => import('../pages/Movies'));
+const MovieDetails = lazy(() => import('../pages/MovieDetails'));
+const Cast = lazy(() => import('./Cast'));
+const Reviews = lazy(() => import('./Reviews'));
 
 export const App = () => {
   return (
